bb041a1adcccc651a7d2fa6b22f3677292e87dce
Shad <farshadkalathingal@gmail.com>
2020-02-29 17:14:48 +0530

    Scrooling Effect

--- js/script.js
+++ js/script.js
@@ -64,24 +64,3 @@ $('.hero-image').mousemove(function(e){
   $(this).css('background-position', moveX + 'px ' + moveY + 'px ');
 })
 
-
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-  if ($(".navbar").offset().top > 50) {
-      $(".navbar-fixed-top").addClass("top-nav-collapse");
-  } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse");
-  }
-});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-  $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-  });
-});
